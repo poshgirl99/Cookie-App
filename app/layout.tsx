fec73be612dcdec81896a85920a0fc15975a1c10
poshgirl99 <poshphoebe97@gmail.com>
2026-08-13 09:10:37 +0000

    Enable instant delete feedback

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import StorySentFeedback from "./story-sent-feedback";
+import InstantDeleteFeedback from "./instant-delete-feedback";
 
 export const metadata: Metadata = {
   title: "Cookie",
@@ -20,6 +21,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       <body>
         {children}
         <StorySentFeedback />
+        <InstantDeleteFeedback />
       </body>
     </html>
   );
